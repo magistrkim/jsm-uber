fc9303486e0286e90d4a62750fc259180e9f0596
Nataliya Kim <magistr1982@gmail.com>
2024-09-08 09:21:29 +0300

    Add assets to the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,19 +8,25 @@ import "react-native-reanimated";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  // const [loaded] = useFonts({
-  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  // });
+const [loaded] = useFonts({
+  "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+  "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+  "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+  "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+  "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+});
 
-  // useEffect(() => {
-  //   if (loaded) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded]);
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
-  // if (!loaded) {
-  //   return null;
-  // }
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <Stack>
