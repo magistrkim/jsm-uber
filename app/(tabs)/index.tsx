00a8461129e30d5fb1faffb2af5e069a0e36c68d
Nataliya Kim <magistr1982@gmail.com>
2024-09-08 08:45:11 +0300

    Add prettier and ESlint to the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,11 @@
-import {
-  View,
-  Text,
-  StatusBar,
-} from "react-native";
+import { Text, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-800">Open up App.js to start working on your app!</Text>
+      <Text className="text-red-800">Uber Clone App!</Text>
       <StatusBar />
     </SafeAreaView>
   );
 }
-
