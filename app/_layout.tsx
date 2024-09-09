fff2be63186d6a9d4d43ed4698a9a72ca1bcb5e3
Nataliya Kim <magistr1982@gmail.com>
2024-09-09 11:47:25 +0300

    Create auth layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,16 +7,16 @@ import "react-native-reanimated";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-const [loaded] = useFonts({
-  "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
-  "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-  "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-  "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
-  "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-  "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
-  "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-});
+const RootLayout = () => {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
 
   useEffect(() => {
     if (loaded) {
@@ -31,7 +31,11 @@ const [loaded] = useFonts({
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
-}
+};
+
+export default RootLayout;
