882d1d977344050ad768d306b23b3d52edd6cb4c
Nataliya Kim <magistr1982@gmail.com>
2024-09-09 12:08:15 +0300

    Create Onboarding UI screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,19 @@
-import { Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
 const Onboarding = () => {
   return (
-    <SafeAreaView>
-      <Text>Onboarding</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        className="w-full flex justify-end items-end p-5"
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+      >
+        <Text className="text-md text-black font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
