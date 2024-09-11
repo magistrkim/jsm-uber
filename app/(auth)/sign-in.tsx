d3c537455e4000a3246a0cd95c0371a4dd5a10c7
Nataliya Kim <magistr1982@gmail.com>
2024-09-11 12:01:25 +0300

    Add sign-in page UI

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,56 @@
-import { Text } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import Button from "@/components/Button";
+import { Link } from "expo-router";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const onSignInPress = async () => {
+    console.log(form.email);
+  };
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-success-600 font-JakartaBold absolute bottom-5 left-5">
+            Sign in
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+            secureTextEntry={true}
+          />
+          <Button title="Sign In" className="mt-6" onPress={onSignInPress} />
+          {/* OAuth */}
+          <Link
+            href={"/(auth)/sign-up"}
+            className="text-lg mt-10 text-center text-general-200"
+          >
+            <Text>Do not have an account? </Text>
+            <Text className="text-general-400">Sign up</Text>
+          </Link>
+        </View>
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
   );
 };
 
