1a51a0b58f674d586780b62dd4a10b01ade69c4f
Nataliya Kim <magistr1982@gmail.com>
2024-09-11 11:53:08 +0300

    Complete sign-up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,20 +1,61 @@
 import { Image, ScrollView, Text, View } from "react-native";
-import React from "react";
-import { images } from "@/constants";
+import React, { useState } from "react";
+import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
+import Button from "@/components/Button";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const onSignUpPress = async () => {
+    console.log(form.name, form.email);
+  };
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
-        <View className="relative w-full h-[300px]">
-          <Image source={images.signUpCar} className="z-0 w-full h-[300px]" />
-          <Text className="text-2xl text-primary-500 font-JakartaSemiBold absolute bottom-5 left-5">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-primary-500 font-JakartaBold absolute bottom-5 left-5">
             Create Your Account
           </Text>
         </View>
         <View className="p-5">
-          <InputField />
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+            secureTextEntry={true}
+          />
+          <Button title="Sign Up" className="mt-6" onPress={onSignUpPress} />
+          {/* OAuth */}
+          <Link
+            href={"/(auth)/sign-in"}
+            className="text-lg mt-10 text-center text-general-200"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-general-400">Log in</Text>
+          </Link>
+          {/* Verification Modal */}
         </View>
       </View>
     </ScrollView>
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,11 +1,54 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from "react-native";
 import React from "react";
+import { InputFieldProps } from "@/types/type";
 
-const InputField = () => {
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  secureTextEntry = false,
+  className,
+  ...props
+}: InputFieldProps) => {
   return (
-    <View>
-      <Text>InputField</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="my-2 w-full">
+          <Text
+            className={`text-lg font-JakartaSemiBold mb-3 text-primary-700 ${labelStyle}`}
+          >
+            {label}
+          </Text>
+          <View
+            className={`flex flex-row justify-start items-center relative bg-primary-200 
+                rounded-2xl border border-primary-200 focus:border-primary-800 ${containerStyle}`}
+          >
+            {icon && (
+              <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
+            )}
+            <TextInput
+              className={`font-JakartaSemiBold rounded-2xl flex-1 p-4 text-[15px] text-left ${inputStyle}`}
+              secureTextEntry={secureTextEntry}
+              {...props}
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
