c2b414df665901ae011d1e306f0270924cad7db7
Nataliya Kim <magistr1982@gmail.com>
2024-09-13 10:42:21 +0300

    Add Clerk authentication and modal

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -5,16 +5,69 @@ import InputField from "@/components/InputField";
 import Button from "@/components/Button";
 import { Link } from "expo-router";
 import OAuth from "@/components/OAuth";
+import { useSignUp } from "@clerk/clerk-expo";
+import ReactNativeModal from "react-native-modal";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
+  const [verification, setVerification] = useState({
+    state: "success",
+    error: "",
+    code: "",
+  });
   const onSignUpPress = async () => {
-    console.log(form.name, form.email);
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerification({ ...verification, state: "pending" });
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
   };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      if (completeSignUp.status === "complete") {
+        // TODO: Create a database USER
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerification({ ...verification, state: "success" });
+      } else {
+        setVerification({
+          ...verification,
+          error: "Verification failed",
+          state: "failed",
+        });
+      }
+    } catch (err: any) {
+      setVerification({
+        ...verification,
+        error: err.errors[0].longMessage,
+        state: "failed",
+      });
+    }
+  };
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -57,7 +110,14 @@ const SignUp = () => {
             <Text className="text-general-400">Log in</Text>
           </Link>
         </View>
-        {/* Verification Modal */}
+        <ReactNativeModal isVisible={verification.state === "success"}>
+          <View className="min-h-[300px] bg-white rounded-2xl px-7 py-9">
+            <Image
+              source={images.check}
+              className="h-[110px] w-[110px]  mx-auto my-5"
+            />
+          </View>
+        </ReactNativeModal>
       </View>
     </ScrollView>
   );
