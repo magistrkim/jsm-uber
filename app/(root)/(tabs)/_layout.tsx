42ce878c6b2c9f74bd4d3a956461c7bb61f9a759
Nataliya Kim <magistr1982@gmail.com>
2024-09-13 11:35:44 +0300

    Complete (tabs) _layout

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <View>
+      <Text>HOMELayout</Text>
+    </View>
+  );
+};
+
+export default Layout;
