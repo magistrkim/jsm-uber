9d7ad3722b5ddfb189b94a3681702278cf61e964
Nataliya Kim <magistr1982@gmail.com>
2024-09-14 11:59:44 +0300

    Add sign-in clerk functionality

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,19 +1,41 @@
 import { Image, ScrollView, Text, View } from "react-native";
-import React, { useState } from "react";
+import { useCallback, useState } from "react";
 import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
 import Button from "@/components/Button";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import OAuth from "@/components/OAuth";
+import { useSignIn } from "@clerk/clerk-react";
 
 const SignIn = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-  const onSignInPress = async () => {
-    console.log(form.email);
-  };
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
