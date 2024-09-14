1f4beb5a61f94f6497daeb04231e6a112e0bca4c
Nataliya Kim <magistr1982@gmail.com>
2024-09-14 12:19:07 +0300

    Create Tabs UI

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,11 +1,65 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "@/constants";
+
+const TabIcon = ({ focused }) => {
+  return (
+    <View className="">
+      <View>
+        <Image />
+      </View>
+    </View>
+  );
+};
 
 const Layout = () => {
   return (
-    <View>
-      <Text>HOMELayout</Text>
-    </View>
+    <Tabs
+      initialRouteName="index"
+      screenOptions={{ tabBarActiveTintColor: "green" }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.home} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.chat} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.profile} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rides"
+        options={{
+          title: "Rides",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} source={icons.list} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
