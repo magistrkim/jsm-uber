ac930cb1efd0730f0394c17a3a3a9f84db0d689a
Nataliya Kim <magistr1982@gmail.com>
2024-09-17 16:11:07 +0300

    Create HomeScreen UI

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,18 @@
 import { SignedIn, useUser } from "@clerk/clerk-expo";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { recentRides } from "@/constants";
+import RideCard from "@/components/RideCard";
 
 const Home = () => {
   const { user } = useUser();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-general-200">
+      <FlatList
+        data={recentRides?.slice(0, 5)}
+        renderItem={({ item }) => <RideCard ride={item} />}
+      />
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
