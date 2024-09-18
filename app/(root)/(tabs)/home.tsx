eeb9a8689a51eb1a524f42dec2d47deb4e7a52a3
Nataliya Kim <magistr1982@gmail.com>
2024-09-18 05:04:16 +0300

    Add Geoapify to the project

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import { SignedIn, useUser } from "@clerk/clerk-expo";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { recentRides } from "@/constants";
 import RideCard from "@/components/RideCard";
@@ -9,13 +9,13 @@ const Home = () => {
 
   return (
     <SafeAreaView className="bg-general-500">
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
       />
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      </SignedIn>
     </SafeAreaView>
   );
 };
--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Ride } from "@/types/type";
 
@@ -17,7 +17,17 @@ const RideCard = ({
   ride: Ride,
 }) => {
   return (
-    <View>
+    <View className="flex flex-row justify-center items-center rounded-lg bg-white shadow-sm shadow-neutral-300 mb-3">
+      <View className="flex flex-row justify-between p-3 items-center">
+        <View className="flex flex-row justify-between items-center">
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:
+              ${destination_longitude}, ${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+          />
+        </View>
+      </View>
       <Text className="text-xl">{destination_address}</Text>
       <Text className="text-2xl text-primary-800">{driver.first_name}</Text>
     </View>
