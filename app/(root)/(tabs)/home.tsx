43d772eed6f9e8d48ad25983ce4fd05cfef69523
Nataliya Kim <magistr1982@gmail.com>
2024-09-19 12:42:45 +0300

    Add Empty Result UI to home screen

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,20 +1,45 @@
-import { SignedIn, useUser } from "@clerk/clerk-expo";
-import { FlatList, Text } from "react-native";
+// import { SignedIn, useUser } from "@clerk/clerk-expo";
+import { FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { recentRides } from "@/constants";
+import { images, recentRides } from "@/constants";
 import RideCard from "@/components/RideCard";
 
 const Home = () => {
-  const { user } = useUser();
+  // const { user } = useUser();
+  const loading = false;
 
   return (
     <SafeAreaView className="bg-general-500">
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-      </SignedIn>
       <FlatList
-        data={recentRides?.slice(0, 5)}
+        // data={recentRides?.slice(0, 5)}
+        data={[]}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col justify-center items-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No recent rides"
+                  resizeMode="contain"
+                />
+                <Text className="text-md font-JakartaMedium text-gray-600">
+                  No recent rides found
+                </Text>
+              </>
+            ) : (
+              <Text className="text-md font-JakartaMedium text-gray-600">
+                Loading
+              </Text>
+            )}
+          </View>
+        )}
       />
     </SafeAreaView>
   );
