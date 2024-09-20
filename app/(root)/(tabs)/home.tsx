4ea2a787005c2ccdc6b09b0a44bd71e01a523e05
Nataliya Kim <magistr1982@gmail.com>
2024-09-20 10:26:51 +0300

    Add SignOut Button to the HomeScreen

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,13 +1,23 @@
 import { useUser } from "@clerk/clerk-expo";
-import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { images, recentRides } from "@/constants";
+import { icons, images, recentRides } from "@/constants";
 import RideCard from "@/components/RideCard";
 
 const Home = () => {
   const { user } = useUser();
   const loading = true;
 
+  const handleSignOut = () => {
+    console.warn(user?.emailAddresses[0].emailAddress);
+  };
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
@@ -42,11 +52,17 @@ const Home = () => {
           </View>
         )}
         ListHeaderComponent={() => (
-          <View className="flex flex-row items-center justify-between my-5">
+          <View className="flex flex-row items-center justify-between my-3">
             <Text className="text-xl font-JakartaExtraBold text-primary-500">
-              Welcome, {user?.firstName || user?.emailAddresses[0].emailAddress}{" "}
-              👋
+              Welcome 👋{" "}
+              {user?.firstName || user?.emailAddresses[0].emailAddress}{" "}
             </Text>
+            <TouchableOpacity
+              onPress={handleSignOut}
+              className="w-10 h-10 justify-center items-center bg-white rounded-md shadow-sm shadow-neutral-400"
+            >
+              <Image source={icons.out} className="w-6 h-6" />
+            </TouchableOpacity>
           </View>
         )}
       />
