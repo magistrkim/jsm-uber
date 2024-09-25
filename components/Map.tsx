cb07735ba850620ab15096946fb622a781775546
Nataliya Kim <magistr1982@gmail.com>
2024-09-25 13:00:33 +0300

    Create map.ts file with functiions

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,23 @@
 import { Text } from "react-native";
 import React from "react";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { useLocationStore } from "@/store";
+import { calculateRegion } from "@/lib/map";
 
 const Map = () => {
+  const {
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
   return (
     <MapView
       provider={PROVIDER_DEFAULT}
