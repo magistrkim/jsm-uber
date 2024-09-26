aed5b304e6c2803d3b339fd8d5d6dbfba084008c
Nataliya Kim <magistr1982@gmail.com>
2024-09-26 09:16:56 +0300

    Add GooglePlacesAutocomplete to GoogleTextInput

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const GoogleTextInput = ({
   icon,
@@ -11,10 +12,41 @@ const GoogleTextInput = ({
 }: GoogleInputProps) => {
   return (
     <View
-      className={`flex flex-row justify-center items-center relative z-50 rounded-md ${containerStyle} mb-5 p-2 px-4`}
+      className={`flex flex-row justify-center items-center relative z-50 rounded-md ${containerStyle} mb-5 px-4`}
     >
       <Image source={icon} className="w-6 h-6" />
-      <Text>SEARCH</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where do you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            width: "100%",
+            top: 0,
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          },
+        }}
+      />
     </View>
   );
 };
