0a4c3a72604eb12186a0e2c533bee17bff1f8bf9
Nataliya Kim <magistr1982@gmail.com>
2024-10-02 09:56:14 +0300

    Create DriverCard component

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import { useLocationStore } from "@/store";
+import RideLayout from "@/components/RideLayout";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import { drivers, icons } from "@/constants";
+import Button from "@/components/Button";
+import { router } from "expo-router";
 
 const ConfirmRide = () => {
+  const { destinationAddress, setDestinationLocation } = useLocationStore();
   return (
-    <View>
-      <Text>ConfirmRide</Text>
-    </View>
+    <RideLayout title="Choose a Driver" snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        renderItem={({ item }) => <Text>DriverCard</Text>}
+      />
+      <View>
+        <Text className="text-lg font-JakartaSemiBold mb-3">To</Text>
+        <GoogleTextInput
+          icon={icons.map}
+          handlePress={(location) => setDestinationLocation(location)}
+          initialLocation={destinationAddress!}
+          containerStyle="bg-white shadow-sm shadow-primary-400"
+          textInputBackgroundColor="transparent"
+        />
+      </View>
+      <Button
+        className="mt-2"
+        title="Book Ride"
+        onPress={() => router.push("/(root)/book-ride")}
+      />
+    </RideLayout>
   );
 };
 
---
+++ components/DriverCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DriverCard = () => {
+  return (
+    <View>
+      <Text>DriverCard</Text>
+    </View>
+  );
+};
+
+export default DriverCard;
