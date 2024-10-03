f03a58649ce5634692e1e86ca0ab75b385c674b9
Nataliya Kim <magistr1982@gmail.com>
2024-10-03 12:07:56 +0300

    Add useDriverStore to the confirm-ride

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,19 +1,24 @@
 import { View, Text, FlatList } from "react-native";
 import React from "react";
-import { useLocationStore } from "@/store";
+import { useDriverStore } from "@/store";
 import RideLayout from "@/components/RideLayout";
-import GoogleTextInput from "@/components/GoogleTextInput";
-import { drivers, icons } from "@/constants";
 import Button from "@/components/Button";
 import { router } from "expo-router";
 import DriverCard from "@/components/DriverCard";
 
 const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
   return (
     <RideLayout title="Choose a Driver" snapPoints={["65%", "85%"]}>
       <FlatList
         data={drivers}
-        renderItem={({ item }) => <DriverCard item={item} />}
+        renderItem={({ item }) => (
+          <DriverCard
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id)}
+            item={item}
+          />
+        )}
         ListFooterComponent={() => (
           <View className="mx-5 mt-8">
             <Button
--- components/Map.tsx
+++ components/Map.tsx
@@ -25,6 +25,7 @@ const Map = () => {
   });
 
   useEffect(() => {
+    setDrivers(drivers);
     if (Array.isArray(drivers)) {
       if (!userLatitude || !userLongitude) return;
       const newMarkers = generateMarkersFromData({
