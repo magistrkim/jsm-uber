a25a1705c247e36b4e63e559efe67763ae026569
Nataliya Kim <magistr1982@gmail.com>
2024-10-07 12:40:09 +0300

    Create Payment component

---
+++ components/Payment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./Button";
+
+const Payment = () => {
+  return (
+    <>
+      <Button title="Pay the Ride" />
+    </>
+  );
+};
+
+export default Payment;
